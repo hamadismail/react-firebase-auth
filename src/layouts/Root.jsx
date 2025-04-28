787fc6a3c05cdcea2b9d82f0e860e6ebe1bfa347
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-28 17:49:02 +0600

    bug-fixing

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../components/Footer';
+import Spiner from '../components/ui/Spiner';
 
 const Root = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+
   return (
     <div>
       <Navbar />
-      <div className="min-h-[calc(100vh-116px)]">
-        <Outlet />
-      </div>
+      {isNavigating ? (
+        <Spiner />
+      ) : (
+        <div className="min-h-[calc(100vh-116px)]">
+          <Outlet />
+        </div>
+      )}
       <Footer />
     </div>
   );

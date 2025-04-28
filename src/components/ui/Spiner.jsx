9df6bd6be277851b28bc291f3f129c705ce498d6
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-28 08:07:01 +0600

    implement-spinner component

---
+++ src/components/ui/Spiner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScaleLoader } from 'react-spinners';
+
+const Spiner = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <ScaleLoader />
+    </div>
+  );
+};
+
+export default Spiner;

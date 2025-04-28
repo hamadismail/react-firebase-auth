da5e6ce0f7aa4953246d677b0a03f1f4cc9452f7
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-28 14:02:13 +0600

    implement show-and-hide-password toggle

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router';
 import GoogleLoginBtn from '../components/ui/GoogleLoginBtn';
 import PasswordIcon from '../components/ui/PasswordIcon';
 import EmailIcon from '../components/ui/EmailIcon';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleSignIn = e => {
     e.preventDefault();
   };
@@ -12,7 +15,7 @@ const SignIn = () => {
   return (
     <div className="card bg-base-100 mt-8 mx-auto max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleSignIn} className="fieldset">
           <label className="label">Email</label>
           <label className="relative">
             <EmailIcon className="absolute top-1/2 z-10 -translate-y-1/2 left-2" />
@@ -27,11 +30,17 @@ const SignIn = () => {
           <label className="relative">
             <PasswordIcon className="absolute top-1/2 z-10 left-2 -translate-y-1/2" />
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               className="input pl-8"
               placeholder="Password"
               required
             />
+            <button
+              className="cursor-pointer absolute top-1/2 -translate-1/2 right-6 text-gray-600"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
           </label>
           <div>
             <a className="link link-hover">Forgot password?</a>

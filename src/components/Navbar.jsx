7cc66d471f2f0143bfa55e22a62f9919f0e36579
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-28 08:24:14 +0600

    implement-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,69 @@
 import React from 'react';
+import { NavLink } from 'react-router';
+import '../styles/navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signin">SignIn</NavLink>
+      </li>
+      <li>
+        <NavLink to="/registration">Registration</NavLink>
+      </li>
+      <li>
+        <NavLink to="/orders">Orders</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar p-0 w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="cursor-pointer btn btn-ghost lg:hidden p-0"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {' '}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{' '}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <a className="cursor-pointer text-xl">React Firebase</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Error from '../pages/Error';
 import Home from '../pages/Home';
 import SignIn from '../pages/SignIn';
 import Registration from '../pages/Registration';
+import Order from '../pages/Order';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { index: true, Component: Home },
       { path: 'signin', Component: SignIn },
       { path: 'registration', Component: Registration },
+      { path: 'orders', Component: Order },
     ],
   },
 ]);

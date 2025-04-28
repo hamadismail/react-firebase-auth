83bbefca255d7c358d2422808efeee5afc86b360
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-28 08:39:48 +0600

    designed-signin and registration page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Registration = () => {
-  return <div>Registration</div>;
+  return (
+    <div className="card bg-base-100 mt-8 mx-auto max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" className="input" placeholder="Name" />
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover"></a>
+          </div>
+          <button className="btn btn-neutral mt-4">Registration</button>
+        </form>
+        <p>
+          ALready have an account?{' '}
+          <Link className="text-blue-500 underline" to="/signin">
+            SignIn
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="card bg-base-100 mt-8 mx-auto max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>
+          Didn't have an account?{' '}
+          <Link className="text-blue-500 underline" to="/registration">
+            Registration
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
